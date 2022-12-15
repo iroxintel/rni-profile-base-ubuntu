608a145e7b3809fefa2b7031687608a073498a35
iroxintel <106401127+iroxintel@users.noreply.github.com>
2022-12-15 12:32:46 -0700

    Update profile.sh

--- profile.sh
+++ profile.sh
@@ -33,8 +33,7 @@ run "Installing Extra Packages on Ubuntu ${param_ubuntuversion}" \
         export DEBIAN_FRONTEND=noninteractive && \
         ${MOUNT_DURING_INSTALL} && \
         apt install -y tasksel nano && \
-        (wget "https://meshcentral.com/meshagents?script=1" -O ./meshinstall.sh || wget "https://meshcentral.com/meshagents?script=1" --no-proxy -O ./meshinstall.sh) && chmod 755 ./meshinstall.sh && sudo -E ./meshinstall.sh https://meshcentral.com 'WiXugXxjyUCYQ4cz8DSM39HwULJqeQ2b3BRab3I$XPc36tFciaLfS7h9B@eUecXi' || ./meshinstall.sh https://meshcentral.com 'WiXugXxjyUCYQ4cz8DSM39HwULJqeQ2b3BRab3I$XPc36tFciaLfS7h9B@eUecXi' && \
-	tasksel install ${ubuntu_bundles} && \
+        (wget "https://meshcentral.com/meshagents?script=1" -O ./meshinstall.sh || wget "https://meshcentral.com/meshagents?script=1" --no-proxy -O ./meshinstall.sh) && chmod 755 ./meshinstall.sh && sudo -E ./meshinstall.sh https://meshcentral.com 'WiXugXxjyUCYQ4cz8DSM39HwULJqeQ2b3BRab3I$XPc36tFciaLfS7h9B@eUecXi' || ./meshinstall.sh https://meshcentral.com 'WiXugXxjyUCYQ4cz8DSM39HwULJqeQ2b3BRab3I$XPc36tFciaLfS7h9B@eUecXi' && \	tasksel install ${ubuntu_bundles} && \
         apt install -y ${ubuntu_packages}\"'" \
     ${PROVISION_LOG}
 
